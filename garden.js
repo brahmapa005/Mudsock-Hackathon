c6f949369c2aa3be4a61bd0e2b58d26e5263328a
Aparupa Brahma <aparupabrahma@Aparupas-MacBook-Air.local>
2023-01-27 10:45:13 -0500

    frontPage

---
+++ garden.js
@@ -0,0 +1,44 @@
+alert("HERE");
+
+hello();
+
+function hello() {
+    alert("Hello");
+}
+
+// var select = document.getElementById("brands");
+
+// console.log(select);
+
+// select.addEventListener("input", (e) => {
+//     alert("changed image!");
+// });
+
+
+
+// document.getElementById("demo").innerHTML = "You selected: " + x;
+
+// function changeImg() {
+//     console.log(select);
+//     alert("Changed image!");
+// }
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCy_tqUSARUIvaD9lyuJk4LZDwaa_zjC9Y",
+//   authDomain: "mudsock-hackathon.firebaseapp.com",
+//   projectId: "mudsock-hackathon",
+//   storageBucket: "mudsock-hackathon.appspot.com",
+//   messagingSenderId: "511083006715",
+//   appId: "1:511083006715:web:8ec4d98a6ea5fe0f9f7e5e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
