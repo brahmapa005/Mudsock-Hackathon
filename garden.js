06585ce889c2d1504b1c0ffbc052b43679cff9a9
Aparupa Brahma <aparupabrahma@Aparupas-MacBook-Air.local>
2023-01-28 12:37:55 -0500

    imgs

--- garden.js
+++ garden.js
@@ -1,44 +1,26 @@
-alert("HERE");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
 
-hello();
+import {
+    doc,
+    setDoc,
+  } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
 
-function hello() {
-    alert("Hello");
-}
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// var select = document.getElementById("brands");
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCy_tqUSARUIvaD9lyuJk4LZDwaa_zjC9Y",
+  authDomain: "mudsock-hackathon.firebaseapp.com",
+  projectId: "mudsock-hackathon",
+  storageBucket: "mudsock-hackathon.appspot.com",
+  messagingSenderId: "511083006715",
+  appId: "1:511083006715:web:8ec4d98a6ea5fe0f9f7e5e"
+};
 
-// console.log(select);
-
-// select.addEventListener("input", (e) => {
-//     alert("changed image!");
-// });
-
-
-
-// document.getElementById("demo").innerHTML = "You selected: " + x;
-
-// function changeImg() {
-//     console.log(select);
-//     alert("Changed image!");
-// }
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCy_tqUSARUIvaD9lyuJk4LZDwaa_zjC9Y",
-//   authDomain: "mudsock-hackathon.firebaseapp.com",
-//   projectId: "mudsock-hackathon",
-//   storageBucket: "mudsock-hackathon.appspot.com",
-//   messagingSenderId: "511083006715",
-//   appId: "1:511083006715:web:8ec4d98a6ea5fe0f9f7e5e"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 
--- hackathonPlantForm.js
+++ hackathonPlantForm.js
@@ -1,33 +1,31 @@
-
-// const plants = {
-//     name: [],
-//     waterFreq: [],
-//     icon: []
-// }
-
-localStorage.setItem("name", ["",""])
-var newName = localStorage.getItem("name");
-console.log(typeof newName)
-// newName.push("pineapple");
-// localStorage.setItem("name", newName);
+//CHOOSING PLANTS//
 
 //icon is set to first one by default
 var selectedIcon = document.getElementById("plant1");
+var imgs = document.querySelectorAll(".plant");
 
+imgs.forEach((img) => {
+    img.addEventListener("click", async(e) => {
+        e.preventDefault();
 
-function selectImage(clickedId){
-    var lastSelectedIcon = selectedIcon;
-    lastSelectedIcon.style.borderColor = "#ffffff";
+        var lastSelectedIcon = selectedIcon;
+        var clickedID = img.id;
+
+        lastSelectedIcon.style.borderColor = "#ffffff";
     
-    selectedIcon = document.getElementById(clickedId);
-    selectedIcon.style.borderColor = "#0689aa"
-    //console.log(selectedIcon);
+        selectedIcon = document.getElementById(clickedID);
+        selectedIcon.style.borderColor = "#0689aa"
+        //console.log(selectedIcon);
 
-    //store the image link
-    var selectedImage = "images/" + clickedId + ".png";
-    //console.log(selectedImage);
-    event.preventDefault();
-}
+        //store the image link
+        var selectedImage = "images/" + clickedID + ".png";
+
+    });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 var nameInput = "";
 var waterInput = "";
@@ -35,25 +33,80 @@ var waterInput = "";
 //save form information when button clicked
 //need to push name, water frequency, and icon to database
 //the image link that needs to be pushed is held in the variable selectedImage
-function save(){
-    //console.log(selectedIcon)
+// function save(){
+//     //console.log(selectedIcon)
+//     nameInput = document.getElementById("nameInput").value;
+//     waterInput = document.getElementById("waterInput").value;
+
+//     await setDoc(doc(db, "Preferences", "4vtPpyJUmneFzmqohQew"), {
+//         name: [nameInput],
+//         time: [timeInput],
+//       });
+
+
+//     // console.log(nameInput)
+//     // console.log(waterInput);
+//     //waterInput = document.getElementById("waterInput").value;
+
+//     // var newName = localStorage.getItem("name");
+//     // newName.push(nameInput);
+//     // localStorage.setItem("name", newName);
+
+//     // plants.name.push(nameInput);
+//     // plants.waterFreq.push(waterInput);
+//     // console.log(plants.name)
+//     console.log(localStorage.getItem("name"));
+
+//     event.preventDefault();
+
+//     // location.href="garden.html";
+// }
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
+
+// import {
+//     doc,
+//     setDoc,
+//   } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCy_tqUSARUIvaD9lyuJk4LZDwaa_zjC9Y",
+//   authDomain: "mudsock-hackathon.firebaseapp.com",
+//   projectId: "mudsock-hackathon",
+//   storageBucket: "mudsock-hackathon.appspot.com",
+//   messagingSenderId: "511083006715",
+//   appId: "1:511083006715:web:8ec4d98a6ea5fe0f9f7e5e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore();
+
+const submitBtn = document.getElementById("subBtn");
+
+submitBtn.addEventListener("click", async(e) => {
+    e.preventDefault();
+
+
+
     nameInput = document.getElementById("nameInput").value;
-    console.log(nameInput)
-    //waterInput = document.getElementById("waterInput").value;
+    waterInput = document.getElementById("waterInput").value;
 
-    // var newName = localStorage.getItem("name");
-    // newName.push(nameInput);
-    // localStorage.setItem("name", newName);
-
-    // plants.name.push(nameInput);
-    // plants.waterFreq.push(waterInput);
-    // console.log(plants.name)
-    console.log(localStorage.getItem("name"));
-
-    event.preventDefault();
-
-    location.href="garden.html";
-}
+    await setDoc(doc(db, "Preferences", "4vtPpyJUmneFzmqohQew"), {
+        name: [nameInput],
+        time: [timeInput],
+      });
+  
+    
+  });
 
 
 
